8218a29e55507021b35a45eeceaeaaa87e794d2f
Raphiiko <iam@raphii.co>
2023-03-31 14:29:15 +0200

    Add mappings for SetBool and GetBool in Settings interface

--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,4 @@
-use std::{ffi::CStr, mem};
+use std::{ffi::CStr, mem::MaybeUninit};
 
 use openvr_sys as sys;
 use Settings;
@@ -7,20 +7,21 @@ use Settings;
 pub struct EVRSettingsError(sys::EVRSettingsError);
 
 impl Settings {
-    pub fn set_float(
+    pub fn set_bool(
         &self,
         pch_section: &CStr,
         pch_settings_key: &CStr,
-        fl_value: f32,
+        b_value: bool,
     ) -> Result<(), EVRSettingsError> {
         unsafe {
-            let mut error: EVRSettingsError = mem::uninitialized();
-            self.0.SetFloat.unwrap()(
+            let mut error: MaybeUninit<EVRSettingsError> = MaybeUninit::uninit();
+            self.0.SetBool.unwrap()(
                 pch_section.as_ptr() as *mut _,
                 pch_settings_key.as_ptr() as *mut _,
-                fl_value,
-                &mut error.0,
+                b_value,
+                error.as_mut_ptr() as *mut sys::EVRSettingsError,
             );
+            let error = error.assume_init();
             if error != EVRSettingsError(sys::EVRSettingsError_VRSettingsError_None) {
                 return Err(error);
             }
@@ -28,14 +29,61 @@ impl Settings {
         }
     }
 
-    pub fn get_float(&self, pch_section: &CStr, pch_settings_key: &CStr) -> Result<f32, EVRSettingsError> {
+    pub fn get_bool(
+        &self,
+        pch_section: &CStr,
+        pch_settings_key: &CStr,
+    ) -> Result<bool, EVRSettingsError> {
         unsafe {
-            let mut error: EVRSettingsError = mem::uninitialized();
+            let mut error: MaybeUninit<EVRSettingsError> = MaybeUninit::uninit();
+            let result = self.0.GetBool.unwrap()(
+                pch_section.as_ptr() as *mut _,
+                pch_settings_key.as_ptr() as *mut _,
+                error.as_mut_ptr() as *mut sys::EVRSettingsError,
+            );
+            let error = error.assume_init();
+            if error != EVRSettingsError(sys::EVRSettingsError_VRSettingsError_None) {
+                return Err(error);
+            }
+            return Ok(result);
+        }
+    }
+
+    pub fn set_float(
+        &self,
+        pch_section: &CStr,
+        pch_settings_key: &CStr,
+        fl_value: f32,
+    ) -> Result<(), EVRSettingsError> {
+        unsafe {
+            let mut error: MaybeUninit<EVRSettingsError> = MaybeUninit::uninit();
+            self.0.SetFloat.unwrap()(
+                pch_section.as_ptr() as *mut _,
+                pch_settings_key.as_ptr() as *mut _,
+                fl_value,
+                error.as_mut_ptr() as *mut sys::EVRSettingsError,
+            );
+            let error = error.assume_init();
+            if error != EVRSettingsError(sys::EVRSettingsError_VRSettingsError_None) {
+                return Err(error);
+            }
+            return Ok(());
+        }
+    }
+
+    pub fn get_float(
+        &self,
+        pch_section: &CStr,
+        pch_settings_key: &CStr,
+    ) -> Result<f32, EVRSettingsError> {
+        unsafe {
+            let mut error: MaybeUninit<EVRSettingsError> = MaybeUninit::uninit();
             let result = self.0.GetFloat.unwrap()(
                 pch_section.as_ptr() as *mut _,
                 pch_settings_key.as_ptr() as *mut _,
-                &mut error.0,
+                error.as_mut_ptr() as *mut sys::EVRSettingsError,
             );
+            let error = error.assume_init();
             if error != EVRSettingsError(sys::EVRSettingsError_VRSettingsError_None) {
                 return Err(error);
             }
